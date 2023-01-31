ac3f3facc1003352ada88e6de41926c8982663c7
d0p1 <contact@d0p1.eu>
2023-01-31 14:08:32 +0100

    refactor javascript

--- assets/js/webring-widget.js
+++ assets/js/webring-widget.js
@@ -1,61 +1,80 @@
-const devse_webring_is_onion = (url) => (url.endsWith(".onion"));
-const devse_webring_is_i2p = (url) => (url.endsWith(".i2p"));
-const devse_webring_get_network = (url) => {
-    if (devse_webring_is_onion(url)) {
-        return "onion";
-    }
+const devseWebringFrom = webring => url => {
 
-    if (devse_webring_is_i2p(url)) {
-        return "i2p";
-    }
+    const urlIs = end => url => url.endsWith(end);
+    const urlIsOnion = urlIs(".onion");
+    const urlIsI2p = urlIs(".i2p");
 
-    return "clearnet";
-}
-const devse_webring_get_network_matching_url = (url, entry) => (entry['protocols']['http'][devse_webring_get_network(url)]);
-const devse_webring_find_member = (url) => (devse_webring_list.findIndex((entry) => {
-    let target = devse_webring_get_network_matching_url(url, entry);
-    console.log(target);
-    return (typeof target === "string" && target.startsWith(url));
-}));
-const devse_webring_get_url_with_fallback = (entry, network) => {
-    http = entry['protocols']['http'];
-    if (network in http) {
-        return http[network];
-    }
+    const getNetworkFromUrl = url => {
+        if (urlIsOnion(url)) {
+            return "onion";
+        }
+        if (urlIsI2p(url)) {
+            return "i2p";
+        }
 
-    return http['clearnet'];
-}
-const devse_webring = (url) => {
-    let element = document.getElementById("devse-webring");
-    let member_id = devse_webring_find_member(url);
+        return "clearnet";
+    };
 
-    if (member_id >= 0) {
-        const network = devse_webring_get_network(url);
-        const random = devse_webring_list[Math.floor(Math.random() * devse_webring_list.length)];
+    const getHttpFromEntry = entry => entry['protocols']['http'];
+    const findMember = url => webring.findIndex(entry => {
+        const network = getNetworkFromUrl(url);
+        const http = getHttpFromEntry(entry);
 
-        const random_url = devse_webring_get_url_with_fallback(random, network);
+        return typeof http[network] === "string" && http[network].startsWith(url);
+    });
 
-        const next = devse_webring_list[(member_id + 1) % devse_webring_list.length];
-        const prev = (member_id - 1) < 0 ? devse_webring_list[devse_webring_list.length - 1] : devse_webring_list[member_id - 1];
+    const getUrlFromNetworkAndEntryWithFallback = network => entry => {
+        const http = getHttpFromEntry(entry);
 
-        const next_url = devse_webring_get_url_with_fallback(next, network);
-        const prev_url = devse_webring_get_url_with_fallback(prev, network);
+        if (network in http) {
+            return http[network];
+        }
 
-        element.insertAdjacentHTML("afterbegin", `
+        return http['clearnet'];
+    };
+
+    const displayHtml = data => {
+        let element = document.getElementById("devse-webring");
+        element.insertAdjacentHTML("afterbegin", data);
+    };
+
+    const getEntry = id => id < 0 ? webring[webring.length - 1] : webring[id % webring.length];
+    const getNext = id => getEntry(id + 1);
+    const getPrev = id => getEntry(id - 1);
+    const getRandom = () => getEntry(Math.floor(Math.random() * webring.length));
+
+    const displayWebring = url => id => {
+        const getUrlFromEntryWithFallback = getUrlFromNetworkAndEntryWithFallback(getNetworkFromUrl(url));
+        const randomUrl = getUrlFromEntryWithFallback(getRandom());
+        const nextUrl = getUrlFromEntryWithFallback(getNext(id));
+        const prevUrl = getUrlFromEntryWithFallback(getPrev(id));
+
+        displayHtml(`
         <ul>
-                <li><a href="${prev_url}">previous</a></li>
-                <li><a href="${random_url}">random</a></li>
-                <li><a href="${next_url}">next</a></li>
+                <li><a href="${prevUrl}">previous</a></li>
+                <li><a href="${randomUrl}">random</a></li>
+                <li><a href="${nextUrl}">next</a></li>
         </ul>
         `);
-    } else {
-        element.insertAdjacentHTML("afterbegin", "This website isn't part of DevSE webring");
-    }
-}
+    };
+
+    const checkMember = url => {
+        const id = findMember(url);
+        if (id >= 0) {
+            return displayWebring(url)(id);
+        }
+
+        displayHtml("This website isn't part of DevSE webring");
+    };
+
+    checkMember(url);
+};
+
+const devseWebring = devseWebringFrom(devse_webring_list);
 
 try {
-    devse_webring(devse_webring_current_url);
+    devseWebring(devse_webring_current_url);
 } catch (e) {
     console.error(e);
-    devse_webring(window.location.origin);
+    devseWebring(window.location.origin);
 }
